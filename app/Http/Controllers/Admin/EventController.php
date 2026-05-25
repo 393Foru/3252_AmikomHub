f8181283bb58f4b039d5d7158eeef2f56eceacb0
393Foru <rahmat.ramadhan.393@gmail.com>
2026-05-25 23:21:27 +0700

    modul praktikum 5 - latihan

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -9,6 +9,59 @@ class EventController extends Controller
 {
     public function index()
     {
-        return view('admin.events');
+        // memakai relasi dan pengaturan limit paginasi (10 entri per halaman)
+        $events = \App\Models\Event::with('category')->latest()->paginate(10);
+        return view('admin.event.index', compact('events'));
+    }
+
+    public function create()
+    {
+        $categories = \App\Models\Category::all();
+        return view('admin.event.create', compact('categories'));
+    }
+
+    public function store(\Illuminate\Http\Request $request)
+    {
+        // menerapkan validasi data request dari pengguna
+        $data = $request->validate([
+            'category_id' => 'required',
+            'title' => 'required|string|max:255',
+            'description' => 'required|date',
+            'location' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+
+        // menyimpan data yang telah divalidasi ke dalam tabel menggunakan Model
+        \App\Models\Event::create($data);
+        return redirect()->route('admin.events.index')->with('success', 'Data Event berhasil ditambahkan.');
+    }
+
+    public function destroy(Event $event)
+    {
+        $event->delete();
+        return redirect()->route('admin.events.index')->with('success', 'Data event berhasil dihapus secara permanen.');
+    }
+
+    public function edit(Event $event)
+    {
+        $categories = \App\Models\Category::all();
+        return view('admin.event.edit', compact('event', 'categories'));
+    }
+
+    public function update(\Illuminate\Http\Request $request, Event $event)
+    {
+        $data = $request->validate([
+            'category_id' => 'required',
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'date' => 'required|date',
+            'location' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+
+        $event->update($data);
+        return redirect()->route('admin.events.index')->with('success', 'Rincian data event berhasil diperbarui.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ use App\Http\Controllers\TicketController;
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\CategoryController as AdminCategoryController; 
-use App\Http\Controllers\Admin\EventController as AdminEventController;
+use App\Http\Controllers\Admin\EventController as EventAdminController;
+
+// use App\Http\Controllers\Admin\EventController as AdminEventController;
 
 // Route User Area
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -19,13 +21,20 @@ Route::get('/checkout/{id}', [EventController::class, 'checkout'])
 Route::get('/tickets', [TicketController::class, 'ticket'])
 ->name('ticket');
 
-// Route Admin Area
-Route::group(['prefix'=>'admin','as'=>'admin.'],function(){
-    Route::get('/', [DashboardController::class, 'index'])
-    ->name('dashboard');
-    Route::get('/events', [AdminEventController::class, 'index'])
-    ->name('events.index');
-    Route::get('/categories', [AdminCategoryController::class, 'index'])
-    ->name('categories.index');
-    //dan seterusnya untuk route admin lainnya...
+
+// Route Admin Area sesuai modul praktikum 5
+Route::prefix('admin')->name('admin.')->group(function ()
+{
+    Route::resource('events, EventAdminController::class');
 });
+
+// // Route Admin Area
+// Route::group(['prefix'=>'admin','as'=>'admin.'],function(){
+//     Route::get('/', [DashboardController::class, 'index'])
+//     ->name('dashboard');
+//     Route::get('/events', [AdminEventController::class, 'index'])
+//     ->name('events.index');
+//     Route::get('/categories', [AdminCategoryController::class, 'index'])
+//     ->name('categories.index');
+//     //dan seterusnya untuk route admin lainnya...
+// });
