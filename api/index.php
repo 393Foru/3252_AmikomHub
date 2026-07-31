0707d7bd4a4cce45ac436e15ac9e5a5dd058b45b
393Foru <rahmat.ramadhan.393@gmail.com>
2026-07-31 19:25:18 +0700

    Fix Laravel 11 caching on Vercel

--- api/index.php
+++ api/index.php
@@ -5,6 +5,11 @@ putenv("CACHE_DRIVER=array");
 putenv("LOG_CHANNEL=stderr");
 putenv("SESSION_DRIVER=cookie");
 putenv("VIEW_COMPILED_PATH=/tmp");
+putenv("APP_CONFIG_CACHE=/tmp/config.php");
+putenv("APP_EVENTS_CACHE=/tmp/events.php");
+putenv("APP_PACKAGES_CACHE=/tmp/packages.php");
+putenv("APP_ROUTES_CACHE=/tmp/routes.php");
+putenv("APP_SERVICES_CACHE=/tmp/services.php");
 $_SERVER['HTTP_ACCEPT'] = 'application/json';
 
 try {
