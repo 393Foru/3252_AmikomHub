0309210fcc787bb820514cc335b678ec810f8458
393Foru <rahmat.ramadhan.393@gmail.com>
2026-07-31 17:41:37 +0700

    catch original error

--- api/index.php
+++ api/index.php
@@ -6,4 +6,12 @@ putenv("VIEW_COMPILED_PATH=/tmp");
 putenv("CACHE_DRIVER=array");
 putenv("LOG_CHANNEL=stderr");
 putenv("SESSION_DRIVER=cookie");
-require __DIR__ . "/../public/index.php";
+try {
+    require __DIR__ . "/../public/index.php";
+} catch (\Throwable $e) {
+    echo "<h1>Original Error:</h1>";
+    echo "<pre>";
+    echo $e->getMessage() . "\n\n";
+    echo $e->getTraceAsString();
+    echo "</pre>";
+}
