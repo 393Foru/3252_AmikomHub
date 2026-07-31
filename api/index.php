f6b0ac451f853d6517fc548481e9bb03cc18af2b
393Foru <rahmat.ramadhan.393@gmail.com>
2026-07-31 17:37:44 +0700

    enable error reporting

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 putenv("VIEW_COMPILED_PATH=/tmp");
 putenv("CACHE_DRIVER=array");
 putenv("LOG_CHANNEL=stderr");
