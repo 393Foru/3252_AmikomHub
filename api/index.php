0686a2df91786a7ac1ad80fbeb19d98c91a52163
393Foru <rahmat.ramadhan.393@gmail.com>
2026-07-31 17:27:59 +0700

    fix: set laravel temp path for vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,6 @@
 <?php
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php';
+putenv("VIEW_COMPILED_PATH=/tmp");
+putenv("CACHE_DRIVER=array");
+putenv("LOG_CHANNEL=stderr");
+putenv("SESSION_DRIVER=cookie");
+require __DIR__ . "/../public/index.php";
